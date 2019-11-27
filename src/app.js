9262711d2fb298ed8d00dafa4b87b621e5161336
matapaska <marta.pasikowska@gmail.com>
2019-11-27 00:01:08 +0000

    MVP

--- src/app.js
+++ src/app.js
@@ -4,7 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      currencies: []
+      currencies: [],
+      exchangeRates:[],
+      exchangeAmount: 0,
+      selectedCurrency: {},
+      exchangeAmountToEuro: 0,
+      selectedCurrencyToEuro: {},
+
+    },
+    computed: {
+      exchangeEuroToCurrency: function () {
+        const value = this.exchangeAmount * this.selectedCurrency
+        return value.toFixed(2);
+      },
+      exchangeCurrencyToEuro: function () {
+        const value = this.exchangeAmountToEuro/this.selectedCurrencyToEuro
+        return value.toFixed(2);
+      }
 
     },
     mounted(){
@@ -15,7 +31,12 @@ document.addEventListener('DOMContentLoaded', () => {
         const request = fetch("https://api.exchangeratesapi.io/latest")
         .then(response => response.json())
         .then(data => this.currencies = data )
+        .then(data => this.exchangeRates = data.rates)
       }
+      // exchangeEuroToCurrency: function () {
+      //   return this.exchangeAmount * this.selectedCurrency.value;
+
+      // }
     }
   })
 })
